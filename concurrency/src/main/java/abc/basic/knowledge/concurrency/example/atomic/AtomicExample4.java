c00f2908b92233de100cdc3f8c7356d276947b42
mortalliao <mortalliaoyujian@163.com>
2020-01-12 17:10:07 +0800

    lock atomic synchronized

---
+++ concurrency/src/main/java/abc/basic/knowledge/concurrency/example/atomic/AtomicExample4.java
@@ -0,0 +1,25 @@
+package abc.basic.knowledge.concurrency.example.atomic;
+
+import abc.basic.knowledge.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Jim
+ */
+@Slf4j
+@ThreadSafe
+public class AtomicExample4 {
+
+    private static AtomicReference<Integer> count = new AtomicReference<>(0);
+
+    public static void main(String[] args) {
+        count.compareAndSet(0, 2); //2
+        count.compareAndSet(0, 1); //no
+        count.compareAndSet(1, 3); //no
+        count.compareAndSet(2, 4); //4
+        count.compareAndSet(3, 5); //no
+        log.info("count:{}", count.get());
+    }
+}
---
+++ concurrency/src/main/java/abc/basic/knowledge/concurrency/example/lock/TestLock.java
@@ -0,0 +1,16 @@
+package abc.basic.knowledge.concurrency.example.lock;
+
+/**
+ * @author Jim
+ *
+ * <pre>
+ *
+ *     原子性: 锁
+ *
+ *     synchronized : 依赖JVM
+ *     Lock : 依赖特殊的CPU指令, 代码实现, ReentrantLock
+ *
+ * </pre>
+ */
+public class TestLock {
+}
