21061b3b4cdc5ce82729f6fba17664f401f8ad00
mortalliao <mortalliaoyujian@163.com>
2020-01-17 14:17:15 +0800

    repeatable annotation

--- concurrent/src/main/java/abc/basic/knowledge/concurrency/TestVolatile.java
+++ concurrent/src/main/java/abc/basic/knowledge/concurrency/TestVolatile.java
@@ -3,16 +3,16 @@ package abc.basic.knowledge.concurrency;
 /**
  * @author Jim
  * <pre>
- * 内存可见性问题是: 当多个线程操作共享数据时, 彼此不可见
  *
- *
+ *      内存可见性问题是: 当多个线程操作共享数据时, 彼此不可见
  *
  * 1. volatile 关键字: 当多个线程进行操作共享数据时, 可以保证内存中的数据可见
  * 相交与synchronized是一种较为轻量级的同步策略
  *
  * 注意:
- * 1. volatile不具备"互斥性"
- * 2. volatile不能保证变量的"原子性"
+ *      1. volatile不具备"互斥性"
+ *      2. volatile不能保证变量的"原子性"
+ *
  * </pre>
  */
 public class TestVolatile {
